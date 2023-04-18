015a4d5536e9a61f809b63db57c2ef8ab5339fac
DhanushM12 <dhanushmukhi@gmail.com>
2023-04-18 21:03:09 +0530

    Updated code

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+import '../App.css';
+import React, {useState} from 'react'
+
+function App() {
+  let time = new Date().toLocaleTimeString()
+  const [currentTime, setCurrentTime] = useState(time)
+  // const [stateName, setterMethod] = useState(initialValue)
+  const updateTime = () => {
+    time = new Date().toLocaleTimeString()
+    setCurrentTime(time)
+  }
+  setInterval(updateTime, 1000);
+  return (
+    <div className='App'>
+      <h1>Todo App</h1>
+      <h2>{ currentTime }</h2>
+    </div>
+  )
+}
+
+export default App
